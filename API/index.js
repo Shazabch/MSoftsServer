feb70e919b0a5e1473907535947f621053d5fde4
ahmi-365 <mohdahmedraza70@gmail.com>
2025-01-17 12:11:51 +0500

    server done

--- API/index.js
+++ API/index.js
@@ -22,25 +22,31 @@ const NewsletterRouter = require('../Routers/Newsletter');
 // Initialize Express app
 const app = express();
 
+// Root route
+app.get('/', (req, res) => {
+  res.send('Welcome to MSofts Server!');
+});
+
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "./Multer/Uploads")));
 app.use(
- session({
-   secret: process.env.SESSION_SECRET || 'defaultSecret', // Use a strong secret
-   resave: false,
-   saveUninitialized: false,
-   store: MongoStore.create({
-     mongoUrl: process.env.MONGO_URI, // MongoDB connection string
-     collectionName: 'sessions',     // Optional: custom session collection name
-   }),
-   cookie: {
-     secure: process.env.NODE_ENV === 'production', // Use HTTPS in production
-     maxAge: 1000 * 60 * 60 * 24, // 1 day
-   },
- })
+  session({
+    secret: process.env.SESSION_SECRET || 'defaultSecret', // Use a strong secret
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI, // MongoDB connection string
+      collectionName: 'sessions',     // Optional: custom session collection name
+    }),
+    cookie: {
+      secure: process.env.NODE_ENV === 'production', // Use HTTPS in production
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+    },
+  })
 );
+
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, {
@@ -67,4 +73,5 @@ const PORT = process.env.PORT || 5000;
 
 // Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 module.exports = app;
